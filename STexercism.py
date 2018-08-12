f693421677460f1a291c14ce13fd7871b3d11c28
Tim Hosgood <timhosgood@googlemail.com>
2018-08-12 16:21:06 +0200

    better error checking for submit + show the success message

--- STexercism.py
+++ STexercism.py
@@ -6,13 +6,33 @@ import re
 
 class SubmitCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        submit_cli = subprocess.check_output(["exercism", "submit", self.view.file_name()])
-        print(submit_cli)
+        try:
+            submit_cli = subprocess.check_output(
+                ["exercism",
+                "submit",
+                self.view.file_name()],
+                stderr=subprocess.STDOUT)
+            sublime.active_window().run_command(
+                "show_panel",
+                {"panel": "console", "toggle": True})
+            print(submit_cli.decode('UTF-8').strip())
+        except subprocess.CalledProcessError as err:
+            raise RuntimeError(
+                "command '{}' returned with error (code {}): {}.".format(
+                    err.cmd,
+                    err.returncode,
+                    err.output.decode('UTF-8').strip())
+                + "\n\nMaybe you submitted the wrong file?")
 
-# TODO: make sure you get the right file? use directory name...
+# TODO: make sure you get the right file? this would be very language dependent
 
 class OpenCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        match = re.search(r'exercism/\w*/([-\w]*)/', self.view.file_name())
+        match = re.search(
+            r'exercism/\w*/([-\w]*)/',
+            self.view.file_name())
         if match:
-            open_cli = subprocess.check_output(["exercism", "open", match.group(1)])
+            open_cli = subprocess.check_output([
+                "exercism",
+                "open",
+                match.group(1)])
