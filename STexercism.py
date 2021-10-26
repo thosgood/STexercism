297195ef4c3283a046fb2c91059ed7ce0d88d364
WeegeeVenom <31588681+WeegeeVenom@users.noreply.github.com>
2021-10-27 07:56:27 +0900

    Added Test + Download
    Added 2 new commands: Test and Download. Test only works for the Python track currently. Download is currently alphanumerical only but is neither case-sensitive nor space-sensitive. (e.g. "Hello World" will work for the exercise name)

--- STexercism.py
+++ STexercism.py
@@ -26,6 +26,7 @@ class StexercismSubmitCurrentFileCommand(sublime_plugin.TextCommand):
 
 # TODO: make sure you get the right file? this would be very language dependent
 
+
 class StexercismOpenCurrentExerciseCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         match = re.search(
@@ -36,3 +37,81 @@ class StexercismOpenCurrentExerciseCommand(sublime_plugin.TextCommand):
                 "exercism",
                 "open",
                 match.group(1)])
+
+class StexercismTestCurrentFilePythonCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        try:
+            submit_cli = subprocess.check_output(
+                ["python",
+                "-m",
+                "pytest",
+                self.view.file_name()[:-3]+"_test.py"],
+                stderr=subprocess.STDOUT)
+            sublime.active_window().run_command(
+                "show_panel",
+                {"panel": "console", "toggle": True})
+            print(submit_cli.decode('UTF-8').strip())
+        
+        except subprocess.CalledProcessError as err:
+            
+            if err.returncode == 1: #This is an exception made for if there are failed tasks
+                print(err.output.decode('UTF-8').strip())
+                pass
+            else:
+                raise RuntimeError(
+                    "command '{}' returned with error (code {}): {}.".format(
+                        err.cmd,
+                        err.returncode,
+                        err.output.decode('UTF-8').strip())
+                    + "\n\nMaybe you are checking the wrong file?")
+
+
+def convert(text):
+    str_list = text.strip().split()
+    return "-".join(str_list).lower()
+
+#TODO: Remove anything that's not alphanumerical?
+
+class StexercismExerciseNameInputHandler(sublime_plugin.TextInputHandler):
+    def name(self):
+        return "exername"
+
+    def placeholder(self):
+        return "Exercise Name"
+
+    def next_input(self, args):
+        if 'trackname' not in args:
+            return StexercismTrackNameInputHandler()
+
+class StexercismTrackNameInputHandler(sublime_plugin.TextInputHandler):
+    def name(self):
+        return "trackname"
+
+    def placeholder(self):
+        return "Track Name"
+
+class StexercismDownloadFileCommand(sublime_plugin.TextCommand):
+    def run(self, edit, exername, trackname): #unfinished
+        try:
+            submit_cli = subprocess.check_output(
+                ["exercism",
+                "download",
+                "--exercise=" + convert(exername),
+                "--track=" + convert(trackname)],
+                stderr=subprocess.STDOUT)
+            sublime.active_window().run_command(
+                "show_panel",
+                {"panel": "console", "toggle": True})
+            print(submit_cli.decode('UTF-8').strip())
+        except subprocess.CalledProcessError as err:
+            raise RuntimeError(
+                "command '{}' returned with error (code {}): {}.".format(
+                    err.cmd,
+                    err.returncode,
+                    err.output.decode('UTF-8').strip()))
+
+    def input(self, args):
+        if 'exername' not in args:
+            return StexercismExerciseNameInputHandler()
+        elif 'trackname' not in args:
+            return StexercismTrackNameInputHandler()
